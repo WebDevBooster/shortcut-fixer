c9dbef8119f6887afbadf210ce6d4ef69068837c
Alex Booster <webdevbooster@gmail.com>
2023-09-23 23:05:30 +0100

    Update to fix bugs.

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 document.addEventListener('keydown', function(e) {
-    if (e.ctrlKey && e.key === 'v') {
+    if (e.ctrlKey && e.key === 'v' || e.key === 'k') {  // 'k' is because in my non-standard keyboard layout my CTRL+V gives a key of 'k'
         // Allow the default paste behavior
         e.stopImmediatePropagation();  // Stops other listeners from receiving this event
     }
